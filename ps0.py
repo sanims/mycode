52af2e0404a1e24386408495149b0a543a7cfa2e
sanims <55282228+sanims@users.noreply.github.com>
2019-09-16 12:40:06 +0100

    Add files via upload

---
+++ ps0.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on Fri Aug 23 08:55:40 2019
+Mat No: U18/FNS/MTH/1061
+@author: Nnaluo_patrick
+"""
+
+import math
+x = input("Enter a number x: ")
+y = input("Enter a number y: ")
+x_power_y = float(x)**float(y)
+print("x**y = " + str(x_power_y))
+#return log(x base(2))
+print("log(x) = " + str(math.log(float(x), 2)))
+
+
+
+
